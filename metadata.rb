7301fc3adf529b169b4acca71f7ac458f91be13f
Greg Albrecht <gba@onbeep.com>
2014-09-12 22:59:46 -0700

    Update metadata.rb
    Renamed to 'unifi', removed dep on ufw.

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,9 @@
-name             'ubnt-unifi'
+name             'unifi'
 maintainer       'Brian Dwyer - Intelligent Digital Services'
 maintainer_email 'BDwyerTech'
 license          'All rights reserved'
 description      'Installs/Configures ubnt-unifi'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '1.0.0'
 
 depends 'apt'
-depends 'ufw'
