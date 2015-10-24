a23dacfd32d4509d73b2a6f9c1c4f684cd547389
Tim Smith <tsmith84@gmail.com>
2015-10-24 00:30:56 -0700

    Use the standard license text
    This lets scanners detect it correctly

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'unifi'
 maintainer       'Greg Albrecht'
 maintainer_email 'gba@onbeep.com'
-license          'Apache License, Version 2.0'
+license          'Apache 2.0'
 description      'Installs/Configures Ubiquiti UniFi server.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '1.0.0'
