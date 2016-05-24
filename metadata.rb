f41ebd62b3874306a2420cc0f63d3ab34529e119
Tim Smith <tsmith@chef.io>
2016-05-24 16:16:18 -0700

    Update contact info in metadata
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'unifi'
-maintainer       'Greg Albrecht'
-maintainer_email 'gba@onbeep.com'
+maintainer       'Tim Smith'
+maintainer_email 'tsmith@chef.io'
 license          'Apache 2.0'
 description      'Installs/Configures Ubiquiti UniFi server.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
@@ -10,3 +10,5 @@ supports 'ubuntu'
 supports 'debian'
 
 depends 'apt'
+source_url 'https://github.com/tas50/unifi' if respond_to?(:source_url)
+issues_url 'https://github.com/tas50/unifi/issues' if respond_to?(:issues_url)
