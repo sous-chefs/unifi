731ba77ac84700161b08cd9d255178779353650a
Tim Smith <tsmith@chef.io>
2016-05-24 16:04:26 -0700

    Take out build version logic

--- metadata.rb
+++ metadata.rb
@@ -4,11 +4,7 @@ maintainer_email 'gba@onbeep.com'
 license          'Apache 2.0'
 description      'Installs/Configures Ubiquiti UniFi server.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-begin
-  version IO.read(File.join(File.dirname(__FILE__), 'VERSION'))
-rescue
-  '1.0.0'
-end
+version '1.0.0'
 
 supports 'ubuntu'
 
