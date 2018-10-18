c3f012114efb4b2d927575bf102ab37a5eef9b1c
Jason Field <jason@avon-lea.co.uk>
2018-10-18 23:13:06 +0100

    CircleCI Fixes
    This should enable the cookbook to build using circleci

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'unifi'
 maintainer       'Tim Smith'
 maintainer_email 'tsmith@chef.io'
-license          'Apache 2.0'
+license          'Apache-2.0'
 description      'Installs/Configures Ubiquiti UniFi server.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.2.0'
