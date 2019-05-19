6752e153d711f906f6675966230e241e2efd2c6e
Jason Field <jason@avon-lea.co.uk>
2019-05-19 10:59:18 +0100

    Fix lint failures

--- metadata.rb
+++ metadata.rb
@@ -1,17 +1,16 @@
-name             'unifi'
-maintainer       'Tim Smith'
-maintainer_email 'tsmith@chef.io'
-license          'Apache-2.0'
-description      'Installs/Configures Ubiquiti UniFi server.'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.2.0'
+name              'unifi'
+maintainer        'Sous Chefs'
+maintainer_email  'help@sous-chefs.org'
+license           'Apache-2.0'
+description       'Installs/Configures Ubiquiti UniFi server.'
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version           '1.2.0'
+source_url        'https://github.com/sous-chefs/unifi'
+issues_url        'https://github.com/sous-chefs/unifi/issues'
+chef_version      '>= 13.0'
 
-supports 'ubuntu'
-supports 'debian'
+%w(ubuntu debian).each do |os|
+  supports os
+end
 
 depends 'java'
-
-source_url 'https://github.com/sous-chefs/unifi'
-issues_url 'https://github.com/sous-chefs/unifi/issues'
-
-chef_version '>= 12.7'
